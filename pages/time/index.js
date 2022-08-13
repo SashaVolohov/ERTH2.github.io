6d8aa9327697fe0b06ebe73f74993f3236a09bae
Artemy Egorov <59178854+artegoser@users.noreply.github.com>
2022-08-13 13:33:06 +0300

    fix: time

--- pages/time/index.js
+++ pages/time/index.js
@@ -11,7 +11,7 @@ setInterval(() => {
 function getStringDate(date) {
   let year = date.getFullYear();
   let month = ("0" + (date.getMonth() + 1)).slice(-2);
-  let day = ("0" + date.getDay()).slice(-2);
+  let day = ("0" + date.getDate()).slice(-2);
   let hours = ("0" + date.getHours()).slice(-2);
   let minutes = ("0" + date.getMinutes()).slice(-2);
   let seconds = ("0" + date.getSeconds()).slice(-2);
